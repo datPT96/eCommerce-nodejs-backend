a19e672f2bb6cebdca4625135a91d0baa0e737ef
datPT196 <mr.klein1996@gmail.com>
2024-09-02 22:58:05 +0700

    session 5: signup & jwt

--- src/app.js
+++ src/app.js
@@ -9,13 +9,19 @@ const app = express()
 app.use(morgan('dev'))
 app.use(helmet())
 app.use(compression())
+app.use(express.json())
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
 
 // init db
 require('./dbs/init.mongodb')
 // const { checkOverLoad } = require('./helpers/check.connect')
 // checkOverLoad()
 // init router
-
+app.use('/', require('./routers'))
 // handle error
 
 module.exports = app
---
+++ src/auth/authUtils.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const JWT = require('jsonwebtoken')
+const createTokenPair = async (payload, publicKey, privateKey) => {
+  try {
+    // accessToken
+    // const accessToken = await JWT.sign(payload, privateKey, {
+    //   algorithm: 'RS256',
+    //   expiresIn: '2 days'
+    // })
+
+    //lv 1,2
+    const accessToken = await JWT.sign(payload, publicKey, {
+      // algorithm: 'RS256',
+      expiresIn: '2 days'
+    })
+
+    const refreshToken = await JWT.sign(payload, privateKey, {
+      // algorithm: 'RS256',
+      expiresIn: '7 days'
+    })
+
+    //
+    JWT.verify(accessToken, publicKey, (err, decode) => {
+      if (err) {
+        console.error(`error verify`)
+      } else {
+        console.log(`decode verify::`, decode)
+      }
+    })
+
+    return {
+      accessToken,
+      refreshToken
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+module.exports = { createTokenPair }
